7611fd2a1ad5db0e03aaca220b5075d15426636e
ZeddBox <jul33anc@gmail.com>
2021-02-08 14:45:30 +0100

    add dark mode and homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
-require('components/dark_mode');
-require('components/dark_mode.js');
-require('@autocomplete-rails');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
